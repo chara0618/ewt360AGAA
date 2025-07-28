e3ef0d84e8681769f86862653fa5d5c4965ca0ac
falseblock <chara0618@proton.me>
2025-07-28 10:25:09 +0800

    v1.0.2

--- run.py
+++ run.py
@@ -5,5 +5,5 @@ from streamlit.web import cli
 if __name__ == '__main__':
     sys.argv[0] = re.sub(r"(-script\.pyw|\.exe)?$", "", sys.argv[0])
     sys.argv.append('run')
-    sys.argv.append('python/main.py')
+    sys.argv.append('./python/main.py')
     sys.exit(cli.main())
